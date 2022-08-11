a547e4194cc50785efbece01d4e3da1c6ae47bdb
Andrey Shpak <anshpak.by@gmail.com>
2022-08-11 20:01:46 +0300

    feat: add simple program which solves quadratic equ

---
+++ quadratic.py
@@ -0,0 +1,10 @@
+import sys, math
+
+b = float(sys.argv[1])
+c = float(sys.argv[2])
+
+d = math.sqrt(b * b - 4 * c)
+
+print('x^2 + ' + sys.argv[1] + 'x + ' + sys.argv[2])
+print('x1 = ' + str((-b + d)/2))
+print('x2 = ' + str((-b - d)/2))
