263767d1efb4aaf54e33166abe8ff7aaf619af14
Andrey Shpak <anshpak.by@gmail.com>
2022-08-12 10:36:03 +0300

    feat: add task answers

---
+++ questions2.py
@@ -0,0 +1,4 @@
+# Упражнение: Выясните, какие еще функции есть в пакете random и с какими еще параметрами можно вызывать random.randrange()?
+import random
+print(dir(random))
+print(random.getrandbits(100))
