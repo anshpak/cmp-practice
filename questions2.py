d37d76e3e65c7e3a99b85d152c216a946c9dc73b
Andrey Shpak <anshpak.by@gmail.com>
2022-08-13 16:34:48 +0300

    feat: add another answers to the lab questions

--- questions2.py
+++ questions2.py
@@ -2,3 +2,16 @@
 import random
 print(dir(random))
 print(random.getrandbits(100))
+
+# Упражнение: Вычислите значение функции sgn(x) для предварительно заданного x используя конструкцию if … elif … else.
+
+import sys
+
+num = int(sys.argv[1])
+
+if num > 0:
+	print(1)
+elif num < 0:
+	print(-1)
+else:
+	print(0)
